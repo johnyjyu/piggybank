9354bb04c5ba8ab4c847f4dbed3a8b489024a9ac
unidh4x0r <cp.unid@gmail.com>
2014-11-15 06:28:44 -0500

    qr code added

--- piggybank.py
+++ piggybank.py
@@ -3,6 +3,7 @@ from blockchain.wallet import Wallet
 import urllib
 import json
 import smtplib
+import webbrowser
 
 def getBTC(id,key):
     wallet = Wallet(id, key)
@@ -19,6 +20,17 @@ def currentUSD(id,key):
     jdata = json.loads(data.read())
     usdVal = jdata['ticker']['sell']
     return usdVal*b
+
+
+def pay(id, key):
+    wallet = Wallet(id, key)
+    add = wallet.list_addresses()
+    newadd = add[0].address
+    #q1 = "https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=" + newadd
+    qr_api = 'https://blockchain.info/qr?data={}&size=300'
+    qr_code = qr_api.format(newadd)
+    webbrowser.open_new(qr_code)
+
 
 def txt(user, password, number, id, key):
     b = getBTC(id, key)
@@ -37,11 +49,8 @@ id1 = '33d99a82-2463-458f-96b5-20fbf0ec36dc'
 key1 = 'piggybank123456789'
 
 
-print getBTC(id1, key1)
-c = currentUSD(id1, key1)
-txt('piggybankbit', 'piggybank12345', 3479937771, id1, key1)
+#print getBTC(id1, key1)
+#c = currentUSD(id1, key1)
+#txt('piggybankbit', 'piggybank12345', 0000000000, id1, key1)
+#pay(id1, key1)
 
-'''commit test
-
-
-'''
